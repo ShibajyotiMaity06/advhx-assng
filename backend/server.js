227ed23f0fa531c7d9387f4943728915ffcfe126
deba-maity <debajyoti.maity29@gmail.com>
2025-08-04 19:05:22 +0530

    Update API endpoint to deployed backenssdaa

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-const DATA_FILE = './jobs.json';
+const path = require('path');
+const DATA_FILE = path.join(__dirname, 'jobs.json');
+
 
 function readData() {
-  return JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
-}
+    return JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
+  }
 function writeData(data) {
   fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
 }
